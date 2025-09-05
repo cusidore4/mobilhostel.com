61e61c81982a98deaac7d2df3a27d7e076b3acba
cusidore4 <occhipintilucio@gmail.com>
2025-09-05 18:37:43 +0200

    update app
    py

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import os
 from supabase import create_client, Client
 
 # Variabili d'ambiente per il database
-supabase_url: str = os.environ.get("SUPABASE_URL")
-supabase_key: str = os.environ.get("SUPABASE_KEY")
+supabase_url: str = os.environ.get("https://plspverzyqbobpfijixv.supabase.co")
+supabase_key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsc3B2ZXJ6eXFib2JwZmlqaXh2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NjkxODEzOCwiZXhwIjoyMDcyNDk0MTM4fQ.nUrbtZohUHnGTS0QqdwgMENr0ZEHg5a0cy6TrXjko3w")
+
+# Aggiungi questa linea per debug:
+print(f"URL Supabase: {supabase_url}")
+
 supabase: Client = create_client(supabase_url, supabase_key)
 
 app = Flask(__name__)
