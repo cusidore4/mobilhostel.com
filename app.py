2f5338bec909f37390cea255a1c1ae1035a83870
cusidore4 <occhipintilucio@gmail.com>
2025-09-05 17:57:23 +0200

    update connessione
    git commit -m "Aggiunto il codice per la connessione a Supabase

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+import os
+from supabase import create_client, Client
+
+# Variabili d'ambiente per il database
+supabase_url: str = os.environ.get("SUPABASE_URL")
+supabase_key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(supabase_url, supabase_key)
 
 app = Flask(__name__)
 
